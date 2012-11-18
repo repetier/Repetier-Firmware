66b3573c2c63c5a4d77c622a49743ae1c1cdbb1d
repetier <darko371@googlemail.com>
2012-11-18 13:24:20 +0100

    Fixed pin names for RAMPS extruder 2 pins

--- Repetier/pins.h
+++ Repetier/pins.h
@@ -272,9 +272,9 @@ STEPPER_CURRENT_CONTROL
 #define E_DIR_PIN          28
 #define E_ENABLE_PIN       24
 
-#define E_1_STEP_PIN         36
-#define E_1_DIR_PIN          34
-#define E_1_ENABLE_PIN       30
+#define E1_STEP_PIN         36
+#define E1_DIR_PIN          34
+#define E1_ENABLE_PIN       30
 
 #define SDPOWER            -1
 #define SDSS               53
