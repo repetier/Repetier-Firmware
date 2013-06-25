1b382a02d0e1186734e097d41a95b3d015ff1e8d
johnoly99 <johnoly99@gmail.com>
2013-06-25 12:47:46 -0400

    fixed fast.io and pins.h
    Fixed fastio.h and pins.h files to work with RAMBo and RRD LCD's

--- Repetier/pins.h
+++ Repetier/pins.h
@@ -1321,7 +1321,7 @@ STEPPER_CURRENT_CONTROL
 #define HEATER_0_PIN   9
 #define TEMP_0_PIN     0
 
-#define HEATER_1_PIN   7
+#define HEATER_1_PIN   -1 //7
 #define TEMP_1_PIN     1
 
 #define HEATER_2_PIN   -1
@@ -1347,7 +1347,7 @@ STEPPER_CURRENT_CONTROL
 #define LED_PIN        13
 #define FAN_PIN        8
 #define PS_ON_PIN      4
-#define KILL_PIN       -1
+#define KILL_PIN       80
 #define SUICIDE_PIN    -1  //PIN that has to be turned on right after start, to keep power flowing.
 
 #define E0_PINS E0_STEP_PIN,E0_DIR_PIN,E0_ENABLE_PIN,E0_MS1_PIN,E0_MS2_PIN,
