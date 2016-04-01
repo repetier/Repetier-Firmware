42566ce9b5bc7f2cc8967f216eb030c83973a604
repetier <darko371@googlemail.com>
2016-04-01 16:01:20 +0200

    Fix ZHOME_HEAT_HEIGHT related compile error

--- src/ArduinoDUE/Repetier/Commands.cpp
+++ src/ArduinoDUE/Repetier/Commands.cpp
@@ -1082,7 +1082,7 @@ void Commands::processGCode(GCode *com) {
             float actTemp[NUM_EXTRUDER];
             for(int i = 0; i < NUM_EXTRUDER; i++)
                 actTemp[i] = extruder[i].tempControl.targetTemperatureC;
-            Printer::moveToReal(IGNORE_COORDINATE,IGNORE_COORDINATE,RMath::max(EEPROM::zProbeHeight(),ZHOME_HEAT_HEIGHT),IGNORE_COORDINATE,Printer::homingFeedrate[Z_AXIS]);
+            Printer::moveToReal(IGNORE_COORDINATE,IGNORE_COORDINATE,RMath::max(EEPROM::zProbeHeight(),static_cast<float>(ZHOME_HEAT_HEIGHT)),IGNORE_COORDINATE,Printer::homingFeedrate[Z_AXIS]);
             Commands::waitUntilEndOfAllMoves();
 #if ZHOME_HEAT_ALL
             for(int i = 0; i < NUM_EXTRUDER; i++) {
