e46f32f2e89f518cf59565f3011e28699442ec78
Remko van der Meulen <remko.van.der.meulen@wolterskluwer.com>
2017-08-25 17:57:42 +0200

    Custom handler for G and M codes

--- src/ArduinoDUE/Repetier/CustomEvents.h
+++ src/ArduinoDUE/Repetier/CustomEvents.h
@@ -1,6 +1,8 @@
 
 extern bool EventUnhandledGCode(GCode *com);
 
+#undef EVENT_UNHANDLED_G_CODE
 #undef EVENT_UNHANDLED_M_CODE
 
+#define EVENT_UNHANDLED_G_CODE(c) EventUnhandledGCode(c)
 #define EVENT_UNHANDLED_M_CODE(c) EventUnhandledGCode(c)
