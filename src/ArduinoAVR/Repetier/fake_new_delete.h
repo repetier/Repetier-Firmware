d6e155a5bfdafc2d6ec89ba4ed15501a5ebd2cca
rickyrockrat <gpib@rickyrockrat.net>
2018-04-08 01:33:13 -0600

    Adding credits

--- src/ArduinoAVR/Repetier/fake_new_delete.h
+++ src/ArduinoAVR/Repetier/fake_new_delete.h
@@ -1,5 +1,8 @@
 #ifndef _FAKE_NEW_DELETE_H_
 #define _FAKE_NEW_DELETE_H_ 1
+/*Credit for this goes to:
+https://www.avrfreaks.net/forum/avr-c-micro-how
+*/
 void * operator new(size_t size);
 void operator delete(void * ptr);
 __extension__ typedef int __guard __attribute__((mode (__DI__)));
