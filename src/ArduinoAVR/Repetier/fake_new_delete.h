d7f56a4c2aa8c5211eb325af7400aa69e391c9ce
rickyrockrat <gpib@rickyrockrat.net>
2018-04-08 05:14:08 -0600

    Adding files so we can run C++ junk on embedded AVR

---
+++ src/ArduinoAVR/Repetier/fake_new_delete.h
@@ -0,0 +1,12 @@
+#ifndef _FAKE_NEW_DELETE_H_
+#define _FAKE_NEW_DELETE_H_ 1
+void * operator new(size_t size);
+void operator delete(void * ptr);
+__extension__ typedef int __guard __attribute__((mode (__DI__)));
+
+extern "C" int __cxa_guard_acquire(__guard *);
+extern "C" void __cxa_guard_release (__guard *);
+extern "C" void __cxa_guard_abort (__guard *);
+extern "C" void __cxa_pure_virtual(void);
+
+#endif
