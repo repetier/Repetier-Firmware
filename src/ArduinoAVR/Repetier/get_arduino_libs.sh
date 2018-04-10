62be13a4e97e7a56cdaed5a89b8ff7b2406d1445
rickyrockrat <gpib@rickyrockrat.net>
2018-04-10 17:28:56 -0600

    Added re_check, path changes past 1.6.8

--- src/ArduinoAVR/Repetier/get_arduino_libs.sh
+++ src/ArduinoAVR/Repetier/get_arduino_libs.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-ARD_VER="1.0.5"
-LAST_OLD="1.0.6"
+# Version of Arduino we want to build against. 1.0.5 is known to work.
+ARD_VER="1.0.6"
 
+# Do not change the variables below. 
+# These mark the point where the directory structure changed.
+LAST_OLD="1.0.6"
+SPI_SRC="1.6.8"
+
+if [ -n "$1" ]; then
+	ARD_VER="$1"
+fi
+RE_CHECK=0
+if [ -n "$2" ]; then # they want to check out another tag, just do the copy.
+	RE_CHECK=1
+fi
 #Which struct we use
 
 # This is to get the files we need from Arduino to build.
@@ -30,10 +42,22 @@ get_arduino_tag () {
 }
 # Just get the few files we need. 
 get_short_arduino_libs() {
-	URL="https://github.com/rickyrockrat/arduino_short.git"
+	#URL="https://github.com/rickyrockrat/arduino_short.git"
+	URL="https://github.com/rickyrockrat/Arduino.hardware.git"
 	clone_arduino
 }
-get_short_arduino_libs
+if [ "1" = "$RE_CHECK" ]; then
+	rm -rf "$odir"
+	if [ ! -e "$idir" ]; then
+		get_short_arduino_libs
+	else
+		cd ard.all
+		git checkout $ARD_VER
+		cd ..
+	fi
+else
+	get_short_arduino_libs
+fi
 mkdir $odir
 #latest version paths:
 #cp ard.all/hardware/arduino/avr/cores/arduino/* $odir
@@ -41,12 +65,20 @@ mkdir $odir
 #cp ard.all/hardware/arduino/avr/libraries/SPI/src/* $odir
 L=$(echo "$LAST_OLD"|tr -d '.')
 A=$(echo "$ARD_VER"|tr -d '.')
+S=$(echo "$SPI_SRC"|tr -d '.')
+
 let l=L
 let a=A
+let s=S
+
 if [ $a -gt $l ]; then
 	PA="ard.all/hardware/arduino/avr/cores/arduino"
 	PM="ard.all/hardware/arduino/avr/variants/mega"
-	PS="ard.all/hardware/arduino/avr/libraries/SPI/src/SPI.h"
+	if [ $a -ge $s ]; then 
+		PS="ard.all/hardware/arduino/avr/libraries/SPI/src/SPI.h"
+	else
+		PS="ard.all/hardware/arduino/avr/libraries/SPI/SPI.h"
+	fi
 else
 	PA="ard.all/hardware/arduino/cores/arduino"
 	PM="ard.all/hardware/arduino/variants/mega"
