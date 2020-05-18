9545ff329eceec6963740fda13be56342cac663e
repetier <darko371@googlemail.com>
2020-05-18 19:00:10 +0200

    Fix pin names for analog pins for rumba

--- src/Repetier/src/boards/STM32F4/boards/rumba32.h
+++ src/Repetier/src/boards/STM32F4/boards/rumba32.h
@@ -100,14 +100,14 @@ Header pins bottom:   14      12       10      8       6         4        2
 #define ORIG_E2_CS_PIN PD1
 
 // Temperature Sensors
-#define TEMP_0_PIN PC_4 // T0
-#define TEMP_2_PIN PC_3 // T1
-#define TEMP_3_PIN PC_2 // T2
-#define TEMP_4_PIN PC_1 // T3
-#define TEMP_1_PIN PC_0 // TB bed
+#define TEMP_0_PIN static_cast<int>(PC_4) // T0
+#define TEMP_2_PIN static_cast<int>(PC_3) // T1
+#define TEMP_3_PIN static_cast<int>(PC_2) // T2
+#define TEMP_4_PIN static_cast<int>(PC_1) // T3
+#define TEMP_1_PIN static_cast<int>(PC_0) // TB bed
 
-#define THERMOCOUPLE_0_PIN PA3 // A10 on EXP3
-#define THERMOCOUPLE_1_PIN PA4 // A9 on EXP3
+#define THERMOCOUPLE_0_PIN static_cast<int>(PA_3) // A10 on EXP3
+#define THERMOCOUPLE_1_PIN static_cast<int>(PA_4) // A9 on EXP3
 
 // Heaters / Fans
 #define HEATER_0_PIN PC6 // E0 Timer 3 Channel 1
