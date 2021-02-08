70b19debbcc31e57e2560ec3c1e1a4696cb6cf12
Moses <moses.github@outlook.com>
2021-02-08 03:47:42 -0500

    Fix - Restore baud & stepper/maxinactivetime on eeprom reset

--- src/Repetier/src/communication/Eeprom.cpp
+++ src/Repetier/src/communication/Eeprom.cpp
@@ -181,11 +181,19 @@ void EEPROM::restoreEEPROMSettingsFromConfiguration() {
     LevelingCorrector::resetEeprom();
     Leveling::resetEeprom();
     GUI::resetEeprom();
+
+    initBaudrate();
+    maxInactiveTime = MAX_INACTIVE_TIME * 1000ul;
+    stepperInactiveTime = STEPPER_INACTIVE_TIME * 1000ul;
+
     markChanged();
 }
 
 void EEPROM::storeDataIntoEEPROM(uint8_t corrupted) {
 #if EEPROM_MODE != 0
+    if (corrupted) {
+        restoreEEPROMSettingsFromConfiguration();
+    }
     Com::printFLN(PSTR("Storing data to eeprom"));
     mode = EEPROMMode::STORE;
     callHandle();
@@ -277,7 +285,7 @@ void EEPROM::init() {
         }
     } else {
         resetRec = true;
-        storeDataIntoEEPROM(storedcheck != check);
+        storeDataIntoEEPROM(!check || (storedcheck != check));
     }
     var1 = getRecoverByte(0);
     var2 = getRecoverByte(1);
